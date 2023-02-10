1679c84463a9909a0f795db537716e265a1586f4
tugi <blueqwinn@gmail.com>
2023-02-10 19:22:06 +0300

    first commit

--- countdown.rb
+++ countdown.rb
@@ -5,4 +5,15 @@ def countdown
 while x < 20
     puts "#{x} is less than 20"
     x += 1
+def   New Year
+    puts  "HAPPY NEW YEAR!"
+  end
 end
+
+def countdown_with_sleep
+    t = 2
+    while t < 10
+        puts "#{t} is less than 10"
+    t  += 2
+end
+
